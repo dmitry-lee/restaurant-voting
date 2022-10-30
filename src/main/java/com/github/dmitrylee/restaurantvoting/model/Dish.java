20f8fb30155009645a07559cdfda591771fc6bdf
dmitry-lee <AlisaLev@1>
2022-10-30 20:52:56 +0300

    change dish unique index

--- src/main/java/com/github/dmitrylee/restaurantvoting/model/Dish.java
+++ src/main/java/com/github/dmitrylee/restaurantvoting/model/Dish.java
@@ -1,7 +1,9 @@
 package com.github.dmitrylee.restaurantvoting.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
@@ -11,27 +13,27 @@ import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "dish", indexes = {@Index(name = "dish_unique_idx", columnList = "name, price", unique = true)})
+@Table(name = "dish", indexes = {@Index(name = "dish_unique_idx", columnList = "restaurant_id, menu_date, name", unique = true)})
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class Dish extends NamedEntity {
 
     @Positive
-    private Integer price;
+    private int price;
 
     @NotNull
     @Column(name = "menu_date", nullable = false, columnDefinition = "date default now()")
     private LocalDate menuDate = LocalDate.now();
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "restaurant_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
-    @JsonBackReference
+    @JsonIgnore
     private Restaurant restaurant;
 
-    public Dish(Integer id, String name, Integer price) {
+    public Dish(Integer id, String name, int price) {
         super(id, name);
         this.price = price;
     }
